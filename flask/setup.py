55230208ce04ebbb88981877822546be8b6ddf52
baraaz95 <archangle.650@gmail.com>
2024-01-05 16:13:23 +0400

    setup edit

--- flask/setup.py
+++ flask/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Whatsapp_hatbot",
+    name="Whatsapp_chatbot",
     version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
